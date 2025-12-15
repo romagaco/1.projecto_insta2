1f1274e49a00d126ace7af771ceb86bae9c9f29f
roberto <roberdex26@gmail.com>
2025-12-15 11:05:48 -0600

    updating styles

--- 1.projecto_insta2.2/instagrans2/instagrans2/settings.py
+++ 1.projecto_insta2.2/instagrans2/instagrans2/settings.py
@@ -111,6 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "instagrans2" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
